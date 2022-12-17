0d48bc3fbdb7dd9e2a1bab8a778e00cd182c3ec7
NovikovIlya <novikov.i.sergeevich@gmail.com>
2022-12-17 20:33:58 +0300

    four commit (front)

--- JaiCRM/Jaimain/static/Jaimain/main.js
+++ JaiCRM/Jaimain/static/Jaimain/main.js
@@ -3,6 +3,7 @@ const inputText = document.querySelector('.input-text')
 const sidebar = document.querySelector('.left-chapters')
 const sliderRight = document.querySelector('.slider__right')
 const left = document.querySelector('#leftchapters')
+const arrow = document.querySelector('.sidebar__arrow-img')
 
 searchBtn.addEventListener('click', SearchEmpty)
 
@@ -14,12 +15,14 @@ function SearchEmpty(e){
 }
 
 
-sidebar.addEventListener('click',nonActive)
+// sidebar.addEventListener('click',nonActive)
+arrow.addEventListener('click',nonActive)
 
 function nonActive(){
     sidebar.classList.toggle('nonActive');
     // sliderRight.classList.toggle('slider__right-nonActive');
     left.classList.toggle('leftchapters-nonActive');
+    sidebar.classList.toggle('back')
 }
 
 
