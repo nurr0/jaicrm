0e1c176ad5715cedb7faca8c1e50dcb7e8365fd0
NovikovIlya <novikov.i.sergeevich@gmail.com>
2023-01-12 11:42:03 +0300

    80 это пиздец нахуй блять я пагинацию сделал хуй знает как я ебал я отдыхать

--- JaiCRM/Jaimain/static/Jaimain/main.js
+++ JaiCRM/Jaimain/static/Jaimain/main.js
@@ -852,28 +852,73 @@ userImg.addEventListener('click',()=>{
 })
 
 //Пагинация
-const partnersblock = document.querySelectorAll('.partners__info')
-const arrayPartnersblock = Array.from(partnersblock)
-console.log(arrayPartnersblock)
+// const partnersblock = document.querySelectorAll('.partners__info')
+// const arrayPartnersblock = Array.from(partnersblock)
+// console.log(arrayPartnersblock)
 
-let liPagination = document.querySelector('#pagination li')
+// let liPagination = document.querySelector('#pagination li')
 
-let notesOnPage = 3;
+// let notesOnPage = 3;
 
-for (let item of liPagination){
-    item.addEventListener('click',function(){
-        //Получаем номер страницы
-        let pageNum = +this.innerHTML;
-        console.log(pageNum)
-        let notes = arrayPartnersblock.slice
-    });
-}
-
-// for (let item of Object.keys(liPagination)){
+// for (let item of liPagination){
 //     item.addEventListener('click',function(){
 //         //Получаем номер страницы
-//         let pageNum = liPagination[+this.innerHTML];
+//         let pageNum = +this.innerHTML;
 //         console.log(pageNum)
-//         let notes = arrayPartnersblock.slice()
+//         let notes = arrayPartnersblock.slice
 //     });
 // }
+
+var count = 8; //всего записей
+var cnt = 4; //сколько отображаем сначала
+var cnt_page = Math.ceil(count / cnt); //кол-во страниц
+
+//выводим список страниц
+var paginator = document.querySelector(".paginator");
+var page = "";
+for (var i = 0; i < cnt_page; i++) {
+  page += "<span data-page=" + i * cnt + "  id=\"page" + (i + 1) + "\">" + (i + 1) + "</span>";
+}
+paginator.innerHTML = page;
+
+//выводим первые записи {cnt}
+var div_num = document.querySelectorAll(".num");
+for (var i = 0; i < div_num.length; i++) {
+  if (i < cnt) {
+    div_num[i].style.display = "block";
+  }
+}
+
+var main_page = document.getElementById("page1");
+main_page.classList.add("paginator_active");
+
+//листаем
+function pagination(event) {
+  var e = event || window.event;
+  var target = e.target;
+  var id = target.id;
+  
+  if (target.tagName.toLowerCase() != "span") return;
+  
+  var num_ = id.substr(4);
+  var data_page = +target.dataset.page;
+  main_page.classList.remove("paginator_active");
+  main_page = document.getElementById(id);
+  main_page.classList.add("paginator_active");
+
+  var j = 0;
+  for (var i = 0; i < div_num.length; i++) {
+    // var data_num = div_num[i].dataset.num;
+    var data_num = div_num[i].querySelector('.btn__text').href[31]
+
+        // document.querySelectorAll(".num")[0].querySelector('.btn__text').href[31]
+    if (data_num <= data_page || data_num >= data_page)
+      div_num[i].style.display = "none";
+
+  }
+  for (var i = data_page; i < div_num.length; i++) {
+    if (j >= cnt) break;
+    div_num[i].style.display = "block";
+    j++;
+  }
+}
